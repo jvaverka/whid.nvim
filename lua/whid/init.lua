d2566f5949117143a9ce26da26e5fe82584990c9
Vaverka <jvaverka@protonmail.com>
2021-08-25 14:06:21 -0400

    get some data in the window and try (unsuccessfully) to add mapping to quit

--- lua/whid/init.lua
+++ lua/whid/init.lua
@@ -20,19 +20,51 @@ local function create_floating_window()
 end
 
 local function update_view()
+  vim.api.nvim_buf_set_option(buf_handler, 'modifiable', true)
   -- we will use vim systemlist function which run shell
   -- command and return result as list
-  local result = vim.fn.systemlist('git diff-tree --no-commit-id --name-only -r HEAD')
+  local result = vim.fn.systemlist('git whatchanged')
+  print(vim.inspect(result))
 
   -- with small indentation results will look better
   for k,v in pairs(result) do
     result[k] = '  '..result[k]
   end
 
-  api.nvim_buf_set_lines(buf_handler, 0, -1, false, result)
+  vim.api.nvim_buf_set_lines(buf_handler, 0, -1, false, result)
+  vim.api.nvim_buf_set_option(buf_handler, 'modifiable', false)
+end
+
+local function close_window()
+  vim.api.nvim_win_close(win, true)
+end
+
+local function set_mappings()
+  local mappings = {
+    q = 'close_window()',
+  }
+
+  for k,v in pairs(mappings) do
+    api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"whid".'..v..'<cr>', {
+        nowait = true, noremap = true, silent = true
+      })
+  end
+  local other_chars = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  }
+  for k,v in ipairs(other_chars) do
+    api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
+    api.nvim_buf_set_keymap(buf, 'n',  '<c-'..v..'>', '', { nowait = true, noremap = true, silent = true })
+  end
+end
+
+local function whid()
+  create_floating_window()
+  update_view(0)
 end
 
 return {
-	create_floating_window = create_floating_window,
-  update_view = update_view
+  whid = whid,
+  close_window = close_window
 }
